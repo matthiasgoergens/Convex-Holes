2775c3de14223a836283381672a2a1d56a176810
Matthias <matthias.goergens@gmail.com>
2009-07-12 01:56:03 +0200

    compiles, but does not work

--- ConvexHoles.hs
+++ ConvexHoles.hs
@@ -20,70 +20,6 @@ import ATools
 
 trace = flip const
 
-data R = Runter | R !(Float) | Hoch
-       deriving (Show, Eq, Ord)
-
-makeR :: Point -> Point -> R
-makeR (Point _ _ 0) _ = error "zero first!"
-makeR _ (Point _ _ 0) = error "zero second!"
-makeR (Point x0r y0r z0r) (Point x1r y1r z1r)
-    = case compare x1 x0 of
-        LT -> error "Nur vorwaerts!"
-        EQ -> case compare y1 y0 of
-                LT -> Runter
-                EQ -> error "Points are equal!"
-                GT -> Hoch
-        GT -> R $ (y1 - y0) / (x1 - x0)
-      where x0 = fromRational (x0r % z0r)
-            x1 = fromRational (x1r % z1r)
-            y0 = fromRational (y0r % z0r)
-            y1 = fromRational (y1r % z1r)
-            
-
--- data Interval x = LOpen x | Closed x x | Here x | UOpen x
---                 | LUOpen
---                   deriving Eq
-
--- extrapolate :: Point -> R -> X -> Maybe Y
--- extrapolate (Point x0 y0) (R r) atX
---     = Just (fromIntegral (atX - x0) * r + y0)
--- extrapolate _ Runter _ = Nothing
--- extrapolate _ Hoch _ = Nothing
-
-
--- meetScan _ (Ray (Point _ Oben) _)
---     = error "meetScan: Ausgang des Strahls ist Oben"
--- meetScan scan (Ray (Point x0 (Y y0)) (Point x1 Unten))
---     | scan == x0 && x0 == x1 
---         = (Unten, Y y0)
---     | otherwise = (Unten, Unten)
--- meetScan scan (Ray (Point x0 (Y y0)) (Point x1 Oben))
---     | scan == x0 && x0 == x1 
---         = (Y y0, Oben)
---     | otherwise = (Oben, Oben)
-
--- allowed scan p@(Point x0 y0) r
---     = case compare scan x0 of
---         LT -> error "allowed: Please only ask when scan > x0."
---         EQ -> case r of
---               Runter -> Just $ LOpen y0
---               R _ -> Just $ Here y0
---               Hoch -> Just $ UOpen y0
---         GT -> do liftM Here $ extrapolate p r scan
-
--- combine :: (Interval Y) -> (Interval Y) -> (Interval Y)
--- combine lowerInter upperInter = 
-
--- prop_makeR p0@(Point x0 y0) p1@(Point x1 y1) 
---     = (x0 < x1) && (y0 /= y1) ==>
---        abs (y1 - y') < 0.001
---     where r = makeR p0 p1
---           Just y' = extrapolate p0 r x1
-
-
-cmp :: Point -> R -> Point -> Ordering
-cmp p0 r pn = compare (makeR p0 pn) r
-
 areaL2 :: Lines -> Maybe Float
 areaL2 [] = Just 0
 areaL2 [_] = Just 0
@@ -113,35 +49,45 @@ areaSup2 p@(Poly lower@(l@(Point lx ly lz):_) upper@(u@(Point ux uy uz):_) inf s
                else areaCPoly2 (CPoly (m:lower) (m:upper))
 
 data TryAdd = TryAdd (Point -> Poly -> Poly)
--- instance Arbitrary TryAdd where
---    arbitrary = oneof [tryAddLower, tryAddUpper]
+instance Show TryAdd where
+    show (TryAdd _) = "TryAdd <fn>"
+instance Arbitrary TryAdd where
+    arbitrary = oneof [return . TryAdd $ tryAddLower, return . TryAdd $ tryAddUpper]
+    shrink _ = [TryAdd tryAddLower, TryAdd tryAddUpper]
 
--- instance Arbitrary Poly where
---     arbitrary = do links <- arbitrary
---                    sized (tryAddN (openNew links))
---         where tryAddN poly 0 = return poly
---               tryAddN poly n = if not $ isOpenAt (rechts poly) poly
---                                then return poly
---                                else do (TryAdd add) <- arbitrary
---                                        p <- arbitrary
---                                        case add p poly of
---                                          Nothing -> tryAddN poly n
---                                          Just poly' -> tryAddN poly' (n-1)
+instance Arbitrary Poly where
+     arbitrary = do links <- arbitrary
+                    sized (tryAddN (openNew links))
+         where tryAddN poly 0 = return poly
+               tryAddN poly n = do (TryAdd add) <- arbitrary
+                                   p <- arbitrary
+                                   tryAddN (add p poly) (n-1)
 
-rechts :: Poly -> X
-rechts (Poly lower upper _ _) = (max `on` (x . head)) lower upper
-links :: Poly -> X
-links (Poly lower upper _ _) = (min `on` (x . last)) lower upper
+rechts :: Poly -> Point
+rechts (Poly lower upper _ _) = maximumBy linksVon $ lower ++ upper
+-- links :: Poly -> Point
+-- links (Poly lower upper _ _) = mininumBy linksVon $ lower upper
 
--- addable :: Point -> Poly -> Bool
--- addable p@(Point x y) (Poly lower@(l:_) upper@(u:_) inf sup)
---     = inf <= infN && supN <= sup
---     where infN = makeR l p
---           supN = makeR u p
+tryAddLower :: Point -> Poly -> Poly
+tryAddLower pN poly@(Poly lower@(l:_) upper@(u:_) inf sup)
+    = if rechts poly `linksVon` pN < GT
+      then case (inside pN inf, inside pN sup) of
+             (True, True) -> (Poly (pN:lower) upper infN sup)
+             _ -> poly
+      else poly
+    where infN = makeH l pN
+          supN = makeH u pN
 
--- tryAddLower :: Integer -> Integer -> Poly -> Poly
--- tryAddLower (Point dx y) poly@(Poly (lower@(l@(Point lx ly):_)) upper inf sup)
---     = if px < lx || (infN < inf)
+tryAddUpper :: Point -> Poly -> Poly
+tryAddUpper pN poly@(Poly lower@(l:_) upper@(u:_) inf sup)
+    = if rechts poly `linksVon` pN < GT
+      then case (inside pN inf, inside pN sup) of
+             (True, True) -> (Poly lower (pN:upper) inf supN)
+             _ -> poly
+      else poly
+    where infN = makeH l pN
+          supN = makeH u pN
+
 --       then Nothing
 --       else Just (Poly (p:lower) upper infN sup)
 --     where infN = makeR l p
@@ -158,12 +104,8 @@ links (Poly lower upper _ _) = (min `on` (x . last)) lower upper
 prop_area p = areaSup2 p >= areaCPoly2 (close p)
 
 
-
-
 type Lines = [Point]
 
---data Poly = Poly {lower :: Lines ,upper :: Lines
---                 ,inf   :: Ray   ,sup   :: Ray}
 data Poly = Poly !Lines !Lines
                  !Halbebene !Halbebene deriving (Ord, Eq)
 
@@ -176,14 +118,6 @@ instance Show Poly where
                                 "\n\t"++show sup ++ "\n\t" ++ show inf
 --                              ++ "\n\tm: "++show (meet (last l) inf (last u) sup)
     
--- meet :: Point -> R -> Point -> R -> Maybe Point
--- meet p0@(Point x0 y0) (R r0) p1@(Point x1 y1) (R r1)
---     | r0 == r1 = Nothing
---     | otherwise = Just $ Point x y
---     where x = (x1*r1 - x0*r0 + y0 - y1) / (r1-r0)
---           y = r0 * (x - x0) + y0
--- meet _ _ _ _ = Nothing
-
 data CPoly = CPoly {l :: !Lines, u :: !Lines}
            deriving (Ord, Eq)
 
@@ -197,28 +131,6 @@ openNew links@(Point x y z) = (Poly [links] [links]
 -- WriterT Trace (State z) a
 
 
-
--- isOpenAt scan poly@(Poly _
---                     _
---                     inf
---                     sup)
---     = let p = Point scan 0 1
---       in inside p inf || inside p sup
-
---       case compare scan (rechts poly) of
---         LT -> True
---         EQ -> if (lx == ux) 
---               then abs (uy - ly) > 1
---               else True
---         GT -> case (inf, sup) of
---              (_, Runter) -> False
---              (Hoch, _) -> False
---              (R _, R _)
---                  -> fromMaybe (error "Should not happen (isClosedAt)") $
---                     do syInf <- liftM floor $ extrapolate l inf scan
---                        sySup <- liftM ceiling $ extrapolate u sup scan
---                        return (syInf < sySup)
---              _ -> True
 
 examine :: Poly -> WriterT [CPoly] (State Float) (Maybe Poly)
 examine p = do bestSeen <- get
